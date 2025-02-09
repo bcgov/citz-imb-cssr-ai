80a8d25007efa3f02ca19187f405ca41d8b49f88
Noor Chasib <noorchasib@gmail.com>
2025-02-09 00:44:12 -0800

    Update Navbar.tsx

--- vite-frontend/src/components/Navbar/Navbar.tsx
+++ vite-frontend/src/components/Navbar/Navbar.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "@/components/Navbar/Navbar.css";
 
+// Navigation bar component that displays the app title and team information
 const Navbar: React.FC = () => {
 	return (
 		<nav className="navbar">
+			{/* Left section with app title/home link */}
 			<div className="navbar-left">
 				<Link to="/" className="navbar-brand">
 					CSSR AI
 				</Link>
 			</div>
+			{/* Right section with team information */}
 			<div className="navbar-right">
 				<span className="navbar-team">IPS Team - IMB - CITZ</span>
 			</div>
