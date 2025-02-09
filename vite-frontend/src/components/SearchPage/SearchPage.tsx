cb1ed8c633e6fd811c0f838ba7cb358494cebdaf
Noor Chasib <noorchasib@gmail.com>
2025-02-09 00:44:26 -0800

    Update SearchPage.tsx

--- vite-frontend/src/components/SearchPage/SearchPage.tsx
+++ vite-frontend/src/components/SearchPage/SearchPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SearchForm } from "@/components/SearchForm/SearchForm";
 import "@/components/SearchPage/SearchPage.css";
 
+// Main search page component that displays the title and search form
 const SearchPage: React.FC = () => {
 	return (
 		<div className="search-page">
